3b4136f8b116777360191d6d30ca6aa4073d808c
endreeikestad <47848934+endreeikestad@users.noreply.github.com>
2023-03-14 17:14:25 +0100

    Add files via upload

---
+++ TestGithubActions/TestGithubActions/MyMath.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestGithubActions
+{
+    public class MyMath
+    {
+        public int Sqr(int a)
+        {
+            return 4;
+        }
+    }
+}
---
+++ TestGithubActions/TheUnitTests/MathTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace TestGithubActions.UnitTests
+{
+    public class MathTests
+    {
+        private MyMath _math;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _math = new MyMath();
+        }
+
+        [Test]
+        public void Sq_2_returns4()
+        {
+            var result = _math.Sqr(2);
+
+            Assert.AreEqual(4, result);
+        }
+
+        [Test]
+        public void Sq_3_returns9()
+        {
+            var result = _math.Sqr(3);
+
+            Assert.AreEqual(9, result);
+        }
+    }
+}
